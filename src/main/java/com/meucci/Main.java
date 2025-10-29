b3d248c2e2d21b2c03b41389a73ea45027c29666
lai2-24 <lai2-24@itismeucci.com>
2025-10-29 13:48:26 +0100

    implementato a meta chi ha vinto

--- src/main/java/com/meucci/Main.java
+++ src/main/java/com/meucci/Main.java
@@ -3,5 +3,27 @@ package com.meucci;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+
+
+
+
     }
+
+    public static char chiHaVinto(char[] array){
+        if(areEqual(array, 0,1,2)){
+            return array[0];
+        }
+
+        return ' ';
+    }
+
+    public static char areEqual(char[] array, int a, int b, int c){
+        if(array[a] == array[b] && array[b] == array[c]){
+            return array[a];
+        }
+        return 'x';
+    }
+
 }
