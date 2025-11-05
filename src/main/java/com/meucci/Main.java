d858f305f925419b418cf4bfa95ebfafe05a2b0b
lai2-24 <lai2-24@itismeucci.com>
2025-11-05 12:57:16 +0100

    server funzionante 1.0

--- src/main/java/com/meucci/Main.java
+++ src/main/java/com/meucci/Main.java
@@ -9,8 +9,10 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        char[] tris = {'0', '0', '0', '0', '0', '0', '0', '0', '0'};
+        char[] tris = { '0', '0', '0', '0', '0', '0', '0', '0', '0' };
+        System.out.println(printChar(tris));
         boolean turnoPrimoGiocatore = true;
+        boolean primoTurno = true;
 
         ServerSocket mioServerSocket = new ServerSocket(3000);
         Socket G1 = mioServerSocket.accept();
@@ -18,7 +20,7 @@ public class Main {
         PrintWriter G1_out = new PrintWriter(G1.getOutputStream(), true);
         System.out.println("Giocatore 1 collegato");
         G1_out.println("WAIT");
-        
+
         Socket G2 = mioServerSocket.accept();
         BufferedReader G2_in = new BufferedReader(new InputStreamReader(G2.getInputStream()));
         PrintWriter G2_out = new PrintWriter(G2.getOutputStream(), true);
@@ -26,53 +28,78 @@ public class Main {
         G1_out.println("READY");
         G2_out.println("READY");
 
-
         while (chiHaVinto(tris) == '0') {
             if (turnoPrimoGiocatore) {
-                char mossaScelta = G1_in.readLine().charAt(0);
-                if(aggiungi(tris, '1', mossaScelta)){
-                    //TODO OK
-                } else {
-                    //TODO KO
-                }
-
-                //TODO 
-
-            }else{
-                //TODO same as p1
-
+                gioca(G1_in, G1_out, G2_out, tris, primoTurno, '1');
+            } else {
+                gioca(G2_in, G2_out, G1_out, tris, primoTurno, '2');
             }
+            primoTurno = false;
             turnoPrimoGiocatore = !turnoPrimoGiocatore;
         }
         switch (chiHaVinto(tris)) {
             case '1':
-            //G1 ha vinto
-                //TODO
+                G1_out.println("W");
+                G2_out.println(printChar(tris) + "L");
                 break;
             case '2':
-            //G2 ha vinto
-                //TODO
+                G2_out.println("W");
+                G1_out.println(printChar(tris) + "L");
                 break;
             case 'P':
-            //Pareggio
-                //TODO
+                if (turnoPrimoGiocatore) {
+                    G1_out.println(printChar(tris) + "P");
+                    G2_out.println("P");
+                } else {
+                    G2_out.println(printChar(tris) + "P");
+                    G1_out.println("P");
+                }
                 break;
             default:
                 break;
-        }       
+        }
 
         mioServerSocket.close();
     }
 
-    private static boolean aggiungi(char[] array, char valore, int pos){
-        if(array[pos] == '0'){
+    private static void gioca(BufferedReader in, PrintWriter out, PrintWriter outD, char[] tris, boolean primoTurno, char player)
+            throws IOException {
+        if (!primoTurno) {
+            out.println(printChar(tris) + " ");
+        }
+
+        while (true) {
+            String mossaScelta = in.readLine();
+            if (mossaScelta == null) {
+                outD.println("DISCONNECTED");
+                return;
+            }
+            if (aggiungi(tris, player, mossaScelta)) {
+                if (chiHaVinto(tris) == '0') {
+                    out.println("OK");
+                }
+                break;
+            }
+            out.println("KO");
+        }
+    }
+
+    private static boolean aggiungi(char[] array, char valore, String posS) {
+        if (posS.length() > 1) {
+            return false;
+        }
+        int pos = posS.charAt(0) - '0';
+        if (pos > 8 || pos < 0) {
+            return false;
+        }
+        if (array[pos] == '0') {
             array[pos] = valore;
             return true;
         }
         return false;
     }
 
-    private static char chiHaVinto(char[] array){
+    private static char chiHaVinto(char[] array) {
         /*
          * '0' Nessuno ha vinto
          * '1' G1 ha vinto
@@ -81,23 +108,23 @@ public class Main {
          */
         char r = '0';
 
-        //orizzontali
-        areEqual(array, r, 0,1,2);
-        areEqual(array, r, 3,4,5);
-        areEqual(array, r, 6,7,8);
+        // orizzontali
+        r = areEqual(array, r, 0, 1, 2);
+        r = areEqual(array, r, 3, 4, 5);
+        r = areEqual(array, r, 6, 7, 8);
 
-        //verticali
-        areEqual(array, r, 0,3,6);
-        areEqual(array, r, 1,4,7);
-        areEqual(array, r, 2,5,8);
+        // verticali
+        r = areEqual(array, r, 0, 3, 6);
+        r = areEqual(array, r, 1, 4, 7);
+        r = areEqual(array, r, 2, 5, 8);
 
-        //diagonali
-        areEqual(array, r, 0,4,8);
-        areEqual(array, r, 2,4,6);
+        // diagonali
+        r = areEqual(array, r, 0, 4, 8);
+        r = areEqual(array, r, 2, 4, 6);
 
-        if(r == '0'){
+        if (r == '0') {
             for (char c : array) {
-                if(c == '0'){
+                if (c == '0') {
                     return r;
                 }
             }
@@ -106,12 +133,21 @@ public class Main {
         return r;
     }
 
-    private static void areEqual(char[] array, char $r,  int a, int b, int c){
-        if(array[a] == array[b] && array[b] == array[c]){
-            if (array[a] != '0'){
-                $r = array[a];
+    private static char areEqual(char[] array, char r, int a, int b, int c) {
+        if (array[a] == array[b] && array[b] == array[c]) {
+            if (array[a] != '0') {
+                r = array[a];
             }
         }
+        return r;
+    }
+
+    private static String printChar(char[] a) {
+        String s = "";
+        for (char c : a) {
+            s = s + c + ",";
+        }
+        return s;
     }
 
 }
